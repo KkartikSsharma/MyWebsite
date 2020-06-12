7650f62c3cc930f468918ccd85c190fc61b324db
KkartikSsharma <sharmakartik879@gmail.com>
2020-06-12 23:38:30 +0530

    Updates

---
+++ precache-manifest.f6ec2a2e91556c489b7c2a07a43a5a0f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f720c41f9c5e8a54d78c291ba28b2956",
+    "url": "/MyWebsite/index.html"
+  },
+  {
+    "revision": "0b434e691c512f30d813",
+    "url": "/MyWebsite/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "75c0f1773f0e5cec192a",
+    "url": "/MyWebsite/static/css/main.cbb98f1c.chunk.css"
+  },
+  {
+    "revision": "0b434e691c512f30d813",
+    "url": "/MyWebsite/static/js/2.1c03b620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MyWebsite/static/js/2.1c03b620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c0f1773f0e5cec192a",
+    "url": "/MyWebsite/static/js/main.788a3ec3.chunk.js"
+  },
+  {
+    "revision": "48baa19570e506720ec3",
+    "url": "/MyWebsite/static/js/runtime-main.da9c3cd1.js"
+  },
+  {
+    "revision": "13aa190cee7fb66d4a2f18d89c93a4c7",
+    "url": "/MyWebsite/static/media/GLogo.13aa190c.png"
+  },
+  {
+    "revision": "1faf0791eafd8d9fdbe24cabfec303ca",
+    "url": "/MyWebsite/static/media/Java.1faf0791.png"
+  },
+  {
+    "revision": "57246b043d0b30530fda3b8664c9ddf1",
+    "url": "/MyWebsite/static/media/PanjabUniversityLogo.57246b04.jpg"
+  },
+  {
+    "revision": "d3bdb7edee0ae306491ab573c46109a5",
+    "url": "/MyWebsite/static/media/ProfilePic.d3bdb7ed.JPG"
+  },
+  {
+    "revision": "c290a443b3f240732252a813d4a6cf65",
+    "url": "/MyWebsite/static/media/Python.c290a443.png"
+  },
+  {
+    "revision": "4603f0d1d6482913bccc9693c93bcf5a",
+    "url": "/MyWebsite/static/media/React.4603f0d1.jpg"
+  },
+  {
+    "revision": "16d59e9f0dea7fb0821090b2a09ee5fd",
+    "url": "/MyWebsite/static/media/ResumePdf.16d59e9f.pdf"
+  },
+  {
+    "revision": "7ce30324de78507ef3bef28bc1e0bd90",
+    "url": "/MyWebsite/static/media/School.7ce30324.jpg"
+  },
+  {
+    "revision": "bcd223ffa4e27154e19c7d827c350044",
+    "url": "/MyWebsite/static/media/StanChart.bcd223ff.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MyWebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
